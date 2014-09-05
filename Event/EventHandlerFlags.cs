971442ac2696ce10ecb5080a6bf686b41299f27c
RawCode <sharpxe@gmail.com>
2014-09-05 23:44:29 +1100

    EventHandlerFlags RC1

--- Event/EventHandlerFlags.cs
+++ Event/EventHandlerFlags.cs
@@ -6,73 +6,60 @@ using System.Text;
 namespace RC.Rimgazer.Event
 {
     /*
+        This enumeration used to describe how handler should work
+
+        Default params are:
+
+        Priority = Normal
+        Activation stage = StageEntry
+        Binding strategy = BindToType
+        AllowExceptions = false
 
     */
+
     [Flags]
-    enum EventHandlerFlags : byte
+    enum EventHandlerFlags : int
     {
-        //default key apply to all default setting
-        //to evoid issues such cases are commented and not allowed to use directly
-        //inside handling code listed cases are reflected by corresponding actions
-        Default         = 0x00,
+        Nothing         = 0x000,
+        //Priority
+        Internal        = 0x001,   //Only single Internal handler is applicable per event
+                                   //If multiple handlers attempt to register - priority is used
+                                   //If same priority - declaring assembly will be checked
+                                   //Handlers declared by same assembly as event have lowest priority
+                                   //If conflict not resolved - NONE will register and exception is thrown
+                                   //This will happen if multiple mods want to edit same event of other mod
+                                   //Handler of this priority always invoked first
+                                   //Internal handler have full control over event processing
+                                   //it should invoke other handlers, process transactions, push changes
+                                   //it may replace event with other object of compatable class
+                                   //EventFallback class is default internal handler for all events
+                                   //By default special method of event is used for this
+        Low             = 0x002,   //Invoked before Normal and High
+        Normal          = 0x000,   //Invoked after Low and before High
+        High            = 0x004,   //Invoked after Low and Normal
+        //Activation stage
+        StageGameplay   = 0x008,   //for types that may not construct at StageEntry
+                                   //types of delayed construction may not register StageEntry events
+        StageEntry      = 0x000,   //by default all types resolved at StageEntry
+        //Binding strategy
+        BindToField     = 0x010,   //Handler will bind to field defined by annotation
+                                   //its up to developer to construct object and place it to given field
+                                   //empty field will cause NPE on event fire
+        BindToSelf      = 0x020,   //Handler will recieve its own instance of hosting type
+        BindToMethod    = 0x040,   //System will pass control to method named in annotation
+                                   //no other actions are performed
+                                   //given method invoked after all normal handlers of priority are registered
+                                   //can be used to deregister unwanter handlers from other mods
+        BindToType      = 0x080,   //Single instance of type constructed and shared by all handlers
 
+        AllowExceptions = 0x100,   //by default system will automatically disable faulty handlers
+                                   //actions "punished" with removal are:
 
-        //Priority subsection default Normal
+                                   //execution longer then 1000ms inside main thread
+                                   //runtime exceptions
+                                   //event fire recursion
 
-        Internal        = 0x01, 
-                            //Only single Internal handler is applicable per event
-                            //If multiple handlers attempt to register - priority is used
-                            //If same priority - declaring assembly will be checked
-                            //Handlers declared by same assembly as event have lowest priority
-                            //If conflict not resolved - NONE will register and exception is thrown
-                            //This will happen if multiple mods want to edit same event of other mod
-
-                            //Handler of this priority always invoked first
-                            //Internal handler have full control over event processing
-                            //it should invoke other handlers, process transactions, push changes
-                            //it may replace event with other object of compatable class
-                            
-                            //EventFallback class is default internal handler for all events
-
-                            //By default special method of event is used for this
-
-        Low             = 0x02, //Invoked before Normal and High
-        //Normal        = 0x00, //Invoked after Low and before High
-        High            = 0x04, //Invoked after Low and Normal
-
-        //activation stage default StageEntry
-        StageGameplay   = 0x08, //for types that may not construct at StageEntry
-                                //types of delayed construction may not register StageEntry events
-        //StageEntry    = 0x00, //by default all types resolved at StageEntry
-
-        //bind strategy default BintToType
-        BindToField     = 0x100, //Handler will bind to field defined by annotation
-                                 //its up to developer to construct object and place it to given field
-                                 //empty field will cause NPE on event fire
-        BindToSelf      = 0x200, //Handler will recieve its own instance of hosting type
-        BindToMethod    = 0x400, //System will pass control to method named in annotation
-                                 //no other actions are performed
-                                 //given method invoked after all normal handlers of priority are registered
-                                 //can be used to deregister unwanter handlers from other mods
-
-        //BindToType    = 0x000, //Single instance of type constructed and shared by all handlers
-
-
-        //tracking features, disabled by default
-        TrackOverflow  = 0x000, //Setting this flag will enable Stackoverflow prediction
-                                //Used for handlers that may (or will) trigger event they registered for
-                                //if handler A is registered for event B had triggered event B from his body
-                                //that new event B instance not passed to handler A again
-                                //still passed to other handlers
-                                //if handler C triggered event B again and also have TrackOverlow flag
-                                //both handlers excluded for next recursion
-                                //recursion tracking always on and can be accessed from EventRuntime
-                                //without this flag faulty handler will reach stackoverflow and fail
-
-        TrackTime = 0x000,      //if set, will report how long handler took to complete
-        
-        
-        Evilbit = 0x000,        //if set, handler allowed to throw exceptions and block main thread
-
+                                   //setting this flag will allow handler to stay, ever if this will cause
+                                   //entire game to crash
     }
 }
